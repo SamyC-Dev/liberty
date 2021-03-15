b5606a361895656d60a9c53bfb7f5e0c9fac8b36
SamyC-Dev <cosamy2@gmail.com>
2021-03-15 12:31:45 +0100

    WIP: Little update

--- client/src/components/SlideShow/Slide.js
+++ client/src/components/SlideShow/Slide.js
@@ -17,17 +17,22 @@ const Slideshow = () => {
         <div className="containerSlide">
             <Slide {...proprietes}>
                 <div className="each-slide">
-                    <div>
-                        <img src="https://i.pinimg.com/236x/79/51/55/7951559b6d74c50ffe5bea83aff0a36b--cool-pictures-yahoo.jpg" alt="img1" />
+                    <div className="slide_card">
+                        <img src="https://image.freepik.com/vecteurs-libre/lunettes-vue-lion-cool-retro_68946-257.jpg" alt="img1" />
                     </div>
                 </div>
                 <div className="each-slide">
-                    <div>
+                    <div className="slide_card">
                         <img src="https://st.depositphotos.com/1146092/2514/i/600/depositphotos_25143517-stock-photo-cool-dog.jpg" alt="img2" />
                     </div>
                 </div>
                 <div className="each-slide">
-                    <div>
+                    <div className="slide_card">
+                        <img src="https://image.freepik.com/vecteurs-libre/lunettes-vue-lion-cool-retro_68946-257.jpg" alt="img3" />
+                    </div>
+                </div>
+                <div className="each-slide">
+                    <div className="slide_card">
                         <img src="https://image.freepik.com/vecteurs-libre/lunettes-vue-lion-cool-retro_68946-257.jpg" alt="img3" />
                     </div>
                 </div>
