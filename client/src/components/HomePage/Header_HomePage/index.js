999ac4a705d600a18833da0cc910ed909f770b2f
SamyC-Dev <cosamy2@gmail.com>
2021-03-26 19:04:51 +0100

    WIP: change Header_HomePage components is user connected or not

--- client/src/components/HomePage/Header_HomePage/index.js
+++ client/src/components/HomePage/Header_HomePage/index.js
@@ -1,18 +1,23 @@
 import "./header_homepage.css";
 
-const HeaderHomePage = () => {
+const HeaderHomePage = (user) => {
+
     return (
         <div className="Header_HomePage">
             <h4>Liberty partager vos photos, vos idées ou réaction du moment...</h4>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit distinctio repudiandae accusamus dolor sit, amet consectetur adipisicing elit. Suscipit distinctio repudiandae accusamus..</p>
-            <div className="button_header_homepage">
-                <button className="btn waves-effect blue" type="submit" name="action">Se connecter
-                    {/* <i className="material-icons right">send</i> */}
-                </button>
-                <button className="btn waves-effect deep-orange lighten-2" type="submit" name="action">S'incrire
+            {user.user ?
+                <h5>Bienvenue <span className="blue-text">{user.user.pseudo}</span> </h5>
+                :
+                <div className="button_header_homepage">
+                    <button className="btn waves-effect blue" type="submit" name="action">Se connecter
+                    </button>
+                    <button className="btn waves-effect deep-orange lighten-2" type="submit" name="action">S'incrire
                     <i className="material-icons right">send</i>
-                </button>
-            </div>
+                    </button>
+                </div>
+            }
+
         </div>
     )
 };
--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,15 +1,18 @@
+import { useSelector } from 'react-redux';
 import HeaderHomePage from './Header_HomePage';
 import BestPosts from './Best_Posts_HomePage';
 import BestAuthor from './Best_Author_HomePage';
 import StartLiberty from './Start_Liberty_HomePage';
 
+
 import "./homePage.css"
 
 
 const HomePage = () => {
+    const User = useSelector((state) => state.user);
     return (
         <div className="homePage_container">
-            <HeaderHomePage />
+            <HeaderHomePage user={User} />
             <BestPosts />
             <BestAuthor />
             <StartLiberty />
