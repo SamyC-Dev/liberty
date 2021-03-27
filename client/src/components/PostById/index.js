ac79d4f1217ba609549611522d94063a28810365
SamyC-Dev <cosamy2@gmail.com>
2021-03-27 15:17:37 +0100

    WIP: Add Component PostById + implement action and state in reducer + implement like and unlike in this components + data dynamique

--- client/src/components/PostById/index.js
+++ client/src/components/PostById/index.js
@@ -2,12 +2,15 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import getPostById from '../../utils/getPostById';
+import likePost from '../../utils/likePost';
+import unlikePosts from '../../utils/unlikePosts';
 import('./postbyid.css');
 
 const PostById = () => {
 
     const { postId } = useParams();
     const Post = useSelector((state) => state.postById);
+    const User_id = useSelector((state) => state.user._id);
     useEffect(() => { getPostById(postId) }, []);
 
 
@@ -15,16 +18,48 @@ const PostById = () => {
         return (
             <div className='postById_container'>
                 <div className="postById_header">
-                    <h2 className='postById__title'>{Post.title}</h2>
-                    <img src={Post.photo} alt="image" />
+                    <div className="postById_header_top">
+                        <h2 className='postById__title'>{Post.title}</h2>
+                        <div className="postById_header_user_element">
+                            <div className="postById_header_user_element_left">
+                                <img src={Post.postedBy.pic} alt="user photo" />
+                                <p className="postById_user">{Post.postedBy.pseudo}</p>
+                            </div>
+                            <div className="postById_header_user_element_right">
+                                <p>il y a 2 jours</p>
+                            </div>
+
+                        </div>
+                        <img src={Post.photo} alt="image" />
+                    </div>
                     <div className="postbyId_header_body">
                         <div className="postById_likeComment_Element">
-                            <p ><i style={{ color: 'red' }} className="material-icons">favorite</i>{Post.likes.length}</p>
                             <p ><i className="material-icons">comment</i>{Post.comments.length}</p>
-                            <p>il y a 2 jours</p>
+                            <p >
+
+                                {Post.likes.includes(User_id)
+                                    ?
+                                    <i
+                                        style={{ color: 'red' }}
+                                        className="material-icons"
+                                        onClick={() => {
+                                            unlikePosts(Post._id)
+                                            document.location.reload();
+                                        }}
+                                    >favorite</i>
+                                    :
+                                    <i
+                                        style={{ color: 'red' }}
+                                        className="material-icons"
+                                        onClick={() => {
+                                            likePost(Post._id)
+                                            document.location.reload();
+                                        }}
+                                    >favorite_border</i>
+                                }{Post.likes.length}
+
+                            </p>
                         </div>
-                        <img src={Post.postedBy.pic} alt="user photo" />
-                        <p className="postById_user">{Post.postedBy.pseudo}</p>
                     </div>
                     <p className="postById_message"></p>
                 </div>
